5f18108e103481eaa0ddb5219284b5f7611358a6
Bakhytgul Akhmetova <32822906+BakhFr@users.noreply.github.com>
2025-04-06 02:35:11 +0500

    [ToDoList]: create json and render buttons

--- ToDoList/src/app/app.component.ts
+++ ToDoList/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { taskList } from '../data.json';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'toDoList';
+  taskList = taskList;
 }
