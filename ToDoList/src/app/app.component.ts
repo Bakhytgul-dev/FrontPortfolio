6cce53120a8332d5b9d87df607051f7f04eb0ec6
Bakhytgul Akhmetova <32822906+BakhFr@users.noreply.github.com>
2025-04-06 12:08:09 +0500

    [taskList] added simple add function

--- ToDoList/src/app/app.component.ts
+++ ToDoList/src/app/app.component.ts
@@ -1,15 +1,45 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  HostListener,
+  Inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { taskList } from '../data.json';
+import { taskList as tasks } from '../data.json';
 import { NgFor } from '@angular/common';
+
+import { LocalService } from '../services/local.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'toDoList';
-  taskList = taskList;
+  taskList: any = [];
+  count = 0;
+
+  constructor(private localeStore: LocalService) {
+  }
+
+  ngOnInit(): void {
+    this.taskList = tasks;
+    this.count = tasks.length;
+    this.localeStore.saveData('taskList', this.taskList);
+  }
+  ngOnDestroy(): void {
+   //this.localeStore.removeData('taskList');
+  }
+
+  @HostListener('click', ['$event.target'])
+  addTask(btn: any) {
+    this.count = this.count + 1;
+    const task = { name: 'Task' + this.count, id: this.count };
+    this.taskList = [...this.taskList, task];
+    this.localeStore.saveData('taskList', this.taskList);
+  }
 }
