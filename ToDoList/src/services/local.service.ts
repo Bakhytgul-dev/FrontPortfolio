2c6c618145b2b9ae90cd943443fe273a77477c4b
Bakhytgul Akhmetova <32822906+BakhFr@users.noreply.github.com>
2025-05-01 10:19:51 +0500

    [toDoList] ssr turn off

--- ToDoList/src/services/local.service.ts
+++ ToDoList/src/services/local.service.ts
@@ -7,6 +7,11 @@ export class LocalService {
   constructor() {}
 
   public saveData(key: string, value: string) {
+    if (typeof window === 'undefined') {
+      console.log('The window object is not available in this environment.');
+    } else {
+      console.log(window.localStorage); // This will throw an error on the server side
+    }
     localStorage.setItem(key, value);
   }
 
